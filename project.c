a10305c4d31d5832633d8f123583fc98aa8eddc3
darius.rotari <darius.rotari@student.upt.ro>
2023-11-26 18:26:12 +0200

    commit for week 9

--- project.c
+++ project.c
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 
 #define PATH_MAX 512
+#define BUFFER_SIZE 1024
 
 //informatii despre un fisier regulat
 void informatii_fisier(int fd, char *filename, char *filename_statistica,const char *nume_director_output){
@@ -250,7 +251,119 @@ void informatii_legatura_simbolica(int fd_link, char *filename_statistica, char 
     
 }
 
-void parcurge_director(const char *cale_director, const char *director_iesire){
+void conversie_tonuri_de_gri(int fd, char *buffer_for_name, const char *director_iesire){
+
+
+    height_and_width(fd,buffer_for_name,director_iesire);
+
+    pid_t pid_gray = fork();//crearea procesului pentru fisiere cu extensia .bmp
+
+    if(pid_gray == -1){
+        perror("error fork.\n");
+        exit(-1);
+    }
+
+    if (pid_gray == 0) {//procesul copil pentru conversia la tonuri de gri
+                
+        int height, width, dataOffset;
+
+        height = getHeight(fd);
+        width = getWidth(fd);
+                    
+        if (lseek(fd, 0, SEEK_SET) < 0){//mutare la pozitia 0 in structura unui fisier .bmp
+            perror("Error to move the file cursor1\n");
+            exit(-1);
+        }
+
+        if (lseek(fd, 10, SEEK_CUR) < 0){//mutare dupa 10 octeti
+            perror("Error to move the file cursor1\n");
+            exit(-1);
+        }
+
+        if(read(fd, &dataOffset, 4) < 0){//prin dataOffset accesez Raster Data
+            perror("Error to read from file!\n");
+            exit(-1);
+        }
+
+        if(lseek(fd,dataOffset,SEEK_SET) < 0){//muta cursorul in locul unde trebuie modificati pixelii
+            perror("Error to move the file cursor3.\n");
+            exit(-1);
+        }
+
+        int red,green,blue;
+        long long i;
+
+        for (i = 0; i < width * height; i++) {
+
+        if (read(fd, &red, 1) < 0){
+            perror("error red.\n");
+            exit(-1);
+        }
+
+        if (read(fd, &green, 1) < 0){
+            perror("error green.\n");
+            exit(-1);
+        }
+
+        if (read(fd, &blue, 1) < 0){
+            perror("error blue.\n");
+            exit(-1);
+        }
+
+        int gray_value = (int)(0.299 * red + 0.587 * green + 0.114 * blue);
+
+        // Seteaza aceeasi valoare de gri pentru toate cele trei culori
+        red = gray_value;
+        green = gray_value;
+        blue = gray_value;
+
+        // Muta cursorul înapoi la poziția inițială
+        if (lseek(fd, -3, SEEK_CUR) < 0) {
+            perror("Error to move the file cursor4.\n");
+            exit(-1);
+        }
+
+        //Scrie noile valori
+        if (write(fd, &red, 1) < 0) {
+            perror("Error to write red value.\n");
+            exit(-1);
+        }
+
+        if (write(fd, &green, 1) < 0) {
+            perror("Error to write green value.\n");
+            exit(-1);
+        }
+
+        if (write(fd, &blue, 1) < 0) {
+            perror("Error to write blue value.\n");
+            exit(-1);
+        }
+                    
+        }
+                   
+        exit(0); // Iesire din procesul copil pentru conversie la tonuri de gri
+
+    }else{//proces parinte
+
+        int status_gray;
+        pid_t terminated_pid_gray = waitpid(pid_gray, &status_gray, 0); // Asteapta terminarea procesului copil pentru conversie
+
+        if (terminated_pid_gray == -1) {
+            perror("Error waiting for child process (gray conversion) to terminate.\n");
+            exit(-1);
+        }
+        if (WIFEXITED(status_gray)) {
+            printf("S-a incheiat procesul cu pid-ul %d si codul %d\n",terminated_pid_gray,WEXITSTATUS(status_gray));
+        }else{
+            perror("procesul copil pentru conversia la tonuri de gri s-a terminat cu eroare.\n");
+            exit(-1);
+        }
+                    
+    }
+
+}
+
+void parcurge_director(const char *cale_director, const char *director_iesire, char caracter){
 
     DIR *director;
     struct dirent *element;
@@ -265,6 +378,7 @@ void parcurge_director(const char *cale_director, const char *director_iesire){
         exit(-1);
     }
 
+    int suma = 0;//suma pentru numarul total de propozitii corecte 
     
     while ((element = readdir(director)) != NULL) {
 
@@ -313,7 +427,7 @@ void parcurge_director(const char *cale_director, const char *director_iesire){
                 informatii_director(elem,buffer_for_name,element->d_name,director_iesire);
                 numar_linii_scrise = numar_linii_scrise + 5;
 
-                parcurge_director(cale_completa,director_iesire);// Parcurge recursiv subdirectorul.
+                parcurge_director(cale_completa,director_iesire,caracter);// Parcurge recursiv subdirectorul.
                 closedir(dir);
             }
             exit(numar_linii_scrise);
@@ -324,9 +438,15 @@ void parcurge_director(const char *cale_director, const char *director_iesire){
         
 
         if (element->d_type == DT_REG) {//este un fisier regulat
-            
             int numar_linii_scrise = 0;
             pid_t pid;
+            int pfd[2];
+
+            if(pipe(pfd) < 0){
+                perror("eroare la crearea pipe-ului.\n");
+                exit(1);
+            }
+
             pid = fork();//crearea procesului
 
             if(pid == -1){
@@ -335,137 +455,124 @@ void parcurge_director(const char *cale_director, const char *director_iesire){
             }
 
             if(pid == 0){//procesul copil
-        
-            int fd = open(cale_completa, O_RDONLY | O_RDWR);//deschid fisierul
-            if (fd < 0) {
-                perror("Eroare la deschiderea fișierului.\n");
-                exit(-1);
-            }
 
-            char buffer_for_name[100];
-            sprintf(buffer_for_name,"%s_statistica.txt",element->d_name);
+                close(pfd[0]);//inchidere capat de citire, procesul va scrie in pipe
 
-            informatii_fisier(fd,element->d_name,buffer_for_name,director_iesire);
-            numar_linii_scrise = numar_linii_scrise + 8;
-            
-            if (strstr(element->d_name, ".bmp") != NULL) {//daca are extensia .bmp
-
-                height_and_width(fd,buffer_for_name,director_iesire);
-                numar_linii_scrise = numar_linii_scrise + 2;
-
-                pid_t pid_gray = fork();//crearea procesului pentru fisiere cu extensia .bmp
-
-                if(pid_gray == -1){
-                    perror("error fork.\n");
+                int fd = open(cale_completa, O_RDONLY | O_RDWR);//deschid fisierul
+                if (fd < 0) {
+                    perror("Eroare la deschiderea fișierului.\n");
                     exit(-1);
                 }
 
-                if (pid_gray == 0) {//procesul copil pentru conversia la tonuri de gri
+                char buffer_for_name[100];
+                sprintf(buffer_for_name,"%s_statistica.txt",element->d_name);
+
+                informatii_fisier(fd,element->d_name,buffer_for_name,director_iesire);
+                numar_linii_scrise = numar_linii_scrise + 8;
+            
+                if (strstr(element->d_name, ".bmp") != NULL) {//daca are extensia .bmp
+
+                    numar_linii_scrise = numar_linii_scrise + 2;
+                    conversie_tonuri_de_gri(fd, buffer_for_name,director_iesire);//convertim imaginea la tonuri de gri
+
+                }else{
+
+                    char buffer[BUFFER_SIZE];
+                    ssize_t bytes_read;
                 
-                    int height, width, dataOffset;
+                    while((bytes_read = read(fd,buffer,sizeof(buffer))) > 0){
 
-                    height = getHeight(fd);
-                    width = getWidth(fd);
-                    
-                    if (lseek(fd, 0, SEEK_SET) < 0){//mutare la pozitia 0 in structura unui fisier .bmp
-                        perror("Error to move the file cursor1\n");
-                        exit(-1);
-                    }
+                        ssize_t bytes_written = 0;
 
-                    if (lseek(fd, 10, SEEK_CUR) < 0){//mutare dupa 10 octeti
-                        perror("Error to move the file cursor1\n");
-                        exit(-1);
-                    }
+                        while(bytes_written < bytes_read){
 
-                    if(read(fd, &dataOffset, 4) < 0){//prin dataOffset accesez Raster Data
-                        perror("Error to read from file!\n");
-                        exit(-1);
-                    }
+                            ssize_t result = write(pfd[1], buffer + bytes_written, bytes_read - bytes_written);
+                            if (result < 0){
+                                perror("error to write in pipe.\n");
+                                exit(1);
+                           }
 
-                    if(lseek(fd,dataOffset,SEEK_SET) < 0){//muta cursorul in locul unde trebuie modificati pixelii
-                        perror("Error to move the file cursor3.\n");
-                        exit(-1);
-                    }
-
-                    int red,green,blue;
-                    long long i;
-
-                    for (i = 0; i < width * height; i++) {
-
-                        if (read(fd, &red, 1) < 0){
-                            perror("error red.\n");
-                            exit(-1);
+                           bytes_written = bytes_written + result;
                         }
-
-                        if (read(fd, &green, 1) < 0){
-                            perror("error green.\n");
-                            exit(-1);
-                        }
-
-                        if (read(fd, &blue, 1) < 0){
-                            perror("error blue.\n");
-                            exit(-1);
-                        }
-
-                        int gray_value = (int)(0.299 * red + 0.587 * green + 0.114 * blue);
-
-                        // Seteaza aceeasi valoare de gri pentru toate cele trei culori
-                        red = gray_value;
-                        green = gray_value;
-                        blue = gray_value;
-
-                        // Muta cursorul înapoi la poziția inițială
-                        if (lseek(fd, -3, SEEK_CUR) < 0) {
-                            perror("Error to move the file cursor4.\n");
-                            exit(-1);
-                        }
-
-                        //Scrie noile valori
-                        if (write(fd, &red, 1) < 0) {
-                            perror("Error to write red value.\n");
-                            exit(-1);
-                        }
-
-                        if (write(fd, &green, 1) < 0) {
-                            perror("Error to write green value.\n");
-                            exit(-1);
-                        }
-
-                        if (write(fd, &blue, 1) < 0) {
-                            perror("Error to write blue value.\n");
-                            exit(-1);
-                        }
-                    
                     }
-                   
-                    exit(0); // Iesire din procesul copil pentru conversie la tonuri de gri
-
-                }else{//proces parinte
-
-                    int status_gray;
-                    pid_t terminated_pid_gray = waitpid(pid_gray, &status_gray, 0); // Asteapta terminarea procesului copil pentru conversie
-
-                    if (terminated_pid_gray == -1) {
-                        perror("Error waiting for child process (gray conversion) to terminate.\n");
-                        exit(-1);
-                    }
-                    if (WIFEXITED(status_gray)) {
-                       printf("S-a incheiat procesul cu pid-ul %d si codul %d\n",terminated_pid_gray,WEXITSTATUS(status_gray));
-                    }else{
-                        perror("procesul copil pentru conversia la tonuri de gri s-a terminat cu eroare.\n");
-                        exit(-1);
-                    }
-                    
                 }
+
+                close(pfd[1]);//inchidere capat de scriere
+               
+                close(fd);
+                exit(numar_linii_scrise);
         
-            }
+            }//proces părinte
+                
+            pid_array[pid_count++] = pid;
+            int pfd2[2];
 
-            close(fd);
-            exit(numar_linii_scrise);
+            if (strstr(element->d_name, ".bmp") == NULL) {//daca NU are extensia .bmp
 
-            }else { // proces părinte
-                pid_array[pid_count++] = pid;
+                pid_t pid2;
+                    
+                if(pipe(pfd2) < 0){
+                    perror("eroare la crearea pipe-ului 2.\n");
+                    exit(1);
+                }
+
+                pid2 = fork();//crearea procesului
+
+                if(pid2 < 0){
+                    perror("Eroare la fork.\n");
+                    exit(1);
+                }
+
+                close(pfd[1]);
+                
+                if(pid2 == 0){//procesul copil
+                    
+                    //redirectare de file descriptori
+                    dup2(pfd2[1],1);//redirectionez iesirea standard spre capatul de scriere al pipe-ului 2
+                    dup2(pfd[0],0);//redirectionez intrarea standard spre capatul de citire al primului pipe
+                       
+                    char caracter_str[2] = {caracter, '\0'};//pun la caracter terminatorul de sir '\0'
+                    
+                    execlp("bash","bash", "script.sh",caracter_str, NULL); //execută script.sh cu argumentul specificat
+
+                    perror("Error executing script.sh.\n");//dacă execlp s-a intors, nu a putut fi rulat script-ul si afișează eroarea
+                    exit(1);
+
+                }
+               
+                int status;
+                waitpid(pid2, &status, 0);//asteapta terminarea procesului copil
+
+                if (!WIFEXITED(status)) {
+                    perror("Procesul copil s-a terminat anormal.\n");
+                    exit(1);
+                }
+
+                close(pfd[0]);
+                close(pfd[1]);
+
+                char *numar_linii_corecte = (char *)malloc(BUFFER_SIZE);//alocare spatiu pentru numarul de linii corecte
+                
+                if(numar_linii_corecte == NULL){
+                    perror("Eroare la alocarea memoriei pentru numar_linii_corecte.\n");
+                    exit(1);
+                }
+
+                ssize_t bytes = read(pfd2[0], numar_linii_corecte,BUFFER_SIZE);
+                
+                if(bytes < 0){
+                    perror("eroare la citirea numarului de linii corecte.\n");
+                    exit(1);
+                }
+                
+                int numar_linii = atoi(numar_linii_corecte);//convertirea sirului intr-un numar intreg
+
+                suma = suma + numar_linii;//adunarea numarului de linii corecte la suma totala
+            
             }
+            close(pfd2[0]);
+            close(pfd2[1]);
+        
         }
 
         if (element->d_type == DT_LNK) {//daca este o legatura simbolica
@@ -503,14 +610,22 @@ void parcurge_director(const char *cale_director, const char *director_iesire){
         }
 
     }
-    
-    // Așteaptă toți copiii
-    for (int i = 0; i < pid_count; ++i) {
+
+    // Așteaptă toți copiii si afiseaza un mesaj in functie de terminarea fiecarui copil
+    for (int i = 0; i < pid_count; i++) {
         int status;
         waitpid(pid_array[i], &status, 0);
 
-        if (!WIFEXITED(status)) {
-            perror("procesul copil s-a terminat cu eroare.\n");
+        if (WIFEXITED(status)) {
+            printf("S-a incheiat procesul cu pid-ul %d si codul %d.\n", pid_array[i], WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("Procesul copil %d s-a încheiat printr-un semnal: %d\n", i, WTERMSIG(status));
+        } else if (WIFSTOPPED(status)) {
+            printf("Procesul copil %d a fost oprit de semnalul: %d\n", i, WSTOPSIG(status));
+        } else if (WIFCONTINUED(status)) {
+            printf("Procesul copil %d a fost reluat\n", i);
+        } else {
+            perror("Eroare necunoscută la încheierea procesului copil.\n");
             exit(-1);
         }
     }
@@ -519,17 +634,22 @@ void parcurge_director(const char *cale_director, const char *director_iesire){
         perror("eroare inchidere director.\n");
         exit(-1);
     }
+
+    printf("Au fost identificate in total %d propozitii corecte care contin caracterul %c.\n",suma,caracter);
+
 }
 
 
 int main(int argc, char *argv[]){
 
-    if (argc != 3){//numar incorect de argumente
+    if (argc != 4){//numar incorect de argumente
         perror("Bad Usage!\n");
         exit(-1);
     }
+
+    char caracter = argv[3][0];
+
+    parcurge_director(argv[1], argv[2], caracter);
     
-    parcurge_director(argv[1], argv[2]);
- 
     return 0;
 }
--- script.sh
+++ script.sh
@@ -8,17 +8,16 @@ fi
 caracter="$1"
 count_lines=0
 
-while IFS= read -r line
-     do
+while IFS= read -r line;
+    do
     # verificare daca o linie incepe cu o litera mare
     if [[ $line =~ ^[A-Z] ]]
         then
         # verificare propozitii corecte
-        if [[ $line =~ ^[A-Za-z0-9\ \,\.\!\?]+[A-Za-z0-9\ \.\!\?]+[\.!\?]$ && ! $line =~ \,.*\si && $line =~ $caracter ]]
+        if [[ $line =~ ^[A-Za-z0-9\ \,\.\!\?]+[A-Za-z0-9\ \.\!\?]+[\.!\?]$ && ! $line =~ \,.*\si && $line =~ $caracter ]];
         then
             ((count_lines++))
         fi
     fi
 done
-
 echo $count_lines
